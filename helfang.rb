09632b0eec95581156e8144846b97d86661e2089
Mason Stewart <masondesu@gmail.com>
2011-07-10 13:49:33 -0400

    Fixed wrong commit

--- helfang.rb
+++ helfang.rb
@@ -1,6 +1,13 @@
 # Get ready to kick ass.
 require 'sinatra'
+require 'Haml'
+require 'sass'
 
 get '/' do
-	"Hey!"
+	haml :index
+end
+
+get '/stylesheets/:name.css' do
+ content_type 'text/css', :charset => 'utf-8'
+ scss(:"stylesheets/#{params[:name]}")
 end

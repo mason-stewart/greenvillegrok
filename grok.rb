177dbcbe40b234a853c8444ac4e7fb8d53163f87
Sean Gaffney <seangaffney@gmail.com>
2011-12-01 22:23:25 -0500

    Add compass_twitter_bootstrap gem and implementation

--- grok.rb
+++ grok.rb
@@ -2,12 +2,17 @@
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'compass'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
 
 get '/' do
-	haml :index
+  haml :index
 end
 
 get '/stylesheets/:name.css' do
- content_type 'text/css', :charset => 'utf-8'
- scss(:"stylesheets/#{params[:name]}")
+  content_type 'text/css', :charset => 'utf-8'
+  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
 end
